279772ddd2f2d15fd1e8cabd12ebce73ed845524
Avinash Gupta <avinash.gupta@vils.ai>
2023-08-20 01:45:05 +0530

    Done

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -3,12 +3,11 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="w-100 px-10 py-4 flex items-center justify-center gap-5">
-      <Link href={"/"}>Demo</Link>
+      <Link href={"https://youtu.be/Mt5aSPYN9XQ"} target="__blank">
+        Demo
+      </Link>
       <Link href={"/"}>Insta.Scraper</Link>
-      <Link
-        href={"https://github.com/tier3guy/threads.scraper"}
-        target="__blank"
-      >
+      <Link href={"https://github.com/tier3guy/insta.scraper"} target="__blank">
         Github
       </Link>
     </div>
